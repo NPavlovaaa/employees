6e41760dad3ff5848773513d3a8f5ea5a45778f4
NPavlovaaa <n.pavlova.web@gmail.com>
2023-08-19 15:08:16 +0300

    Server Fix config, add routers

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+require('dotenv').config()
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/users', require('./routes/users'));
+
+module.exports = app;
