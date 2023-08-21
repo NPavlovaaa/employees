1b5aedbfa035b9c5981393ea78679ba4631518d9
NPavlovaaa <n.pavlova.web@gmail.com>
2023-08-21 22:01:07 +0300

    Client Create client app, add paths

---
+++ client/src/paths.ts
@@ -0,0 +1,10 @@
+export const Paths = {
+    home: '/',
+    employee: '/employee',
+    employeeAdd: '/employee/add',
+    employeeRemove: '/employee/remove',
+    employeeEdit: '/employee/edit',
+    status: '/status',
+    login: '/login',
+    register: 'register'
+} as const;
